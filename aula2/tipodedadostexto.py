422be66f2597a67391ec56963382c341cb685b2f
aureliosenac <auferleite@bol.com.br>
2024-09-11 10:47:27 -0300

    Add files via upload

---
+++ aula2/tipodedadostexto.py
@@ -0,0 +1,8 @@
+meu_texto = "Senac Botafogo"
+print(meu_texto)
+
+# Podemos juntar
+print(f'Estou estudando no {meu_texto} agora')
+print(meu_texto.upper()) # maiúsculo
+print(meu_texto.lower()) # minusculo
+print(meu_texto.capitalize()) # primeira letra em maiúsculo
