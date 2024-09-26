291126cd8d8e7387b339b725096ff051f1716b06
aureliosenac <auferleite@bol.com.br>
2024-09-26 16:17:58 -0300

    Delete trabalho de casa/trabalho de casa2 directory

--- trabalho de casa/trabalho de casa2/1 - aumenta salario.py
+++
@@ -1,6 +0,0 @@
-salario = float(input("digite o salario "))
-p_aumento = float(input("digite o percentual de aumento "))
-aumento = salario * p_aumento / 100
-novo_salario = salario + aumento
-print("salario anterior: R$ ", salario)
-print("novo salario    : R$ ", novo_salario) 
--- trabalho de casa/trabalho de casa2/4 - tempoviagem.py
+++
@@ -1,4 +0,0 @@
-distancia = int(input("digite a distancia - km "))
-tempo = int(input("digite o tempo gasto - horas "))
-vmedia = distancia / tempo
-print("a velocidade media é de ", vmedia," km/h")
