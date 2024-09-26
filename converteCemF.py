2c2a09d349c849571242c78ff2759b901908b812
aureliosenac <auferleite@bol.com.br>
2024-09-26 16:27:57 -0300

    Delete converteCemF.py

--- converteCemF.py
+++
@@ -1,5 +0,0 @@
-# converte graus celsius em fahrenheit
-celsius = float(input("digite o valor em graus celsius "))
-fahrenheit = (9 * celsius)  / 5 + 32
-print("graus celsius", celsius)
-print("equivalem a ", fahrenheit, " graus fahrenheit")
