5c87cc658ac00de185a413817e68479cd9f4b235
aureliosenac <auferleite@bol.com.br>
2024-10-01 19:54:44 -0300

    Add files via upload

---
+++ Python - exercicios/programa10.py
@@ -0,0 +1,17 @@
+# programa recebe dois numeros inteiros
+# e imprime intervalo entre eles.
+
+num1 =int(input("digite um numero inteiro "))
+num2 =int(input("digite outro numero inteiro "))
+intervalo =int(num2 - num1)
+n1 = num1 + 1
+n2 = num2 - 1
+condicao = True
+while condicao:
+   print(n1, end=' ')
+   n1 = n1 + 1
+   if n1 == num2:
+      condicao = False
+      
+    
+    
